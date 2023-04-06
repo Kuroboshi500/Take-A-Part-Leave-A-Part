dccce5a00968cd28bde59421e708e85846645476
Brian Tran <ninjapika500@outlook.com>
2023-04-06 16:02:01 -0700

    base for the health of both player and enemy will have

---
+++ Assets/TAPALAPA/Scripts/EnemyHealth.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1, 100)]
+    private int _health = 100;
+    public float totalHealth => _health;
+    public event UnityAction onEnemyDeath;
+    public event UnityAction<int> onEnemyDamaged;
+
+
+    void onEnemyHit(var attacker, float dmg)
+    {
+        _health -= (int)Mathf.Round(dmg);
+        Debug.Log("Enemy Current damage taken " + dmg + "\n Current Health is: " + _health);
+
+        onEnemyDamaged?.Invoke(_health);
+        if (_health <= 0)
+        {
+            Debug.Log("Death to enemy");
+            onEnemyDeath?.Invoke();
+        }
+    }
+
+    private void OnEnable()
+    {
+
+    }
+    private void OnDisable()
+    {
+
+    }
+}
---
+++ Assets/TAPALAPA/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1, 100)]
+    private int _health = 50;
+    public float totalHealth => _health;
+    public event UnityAction onPlayerDeath;
+    public event UnityAction<int> onPlayerDamaged;
+    
+
+    void onPlayerHit(var attacker, float dmg)
+    {
+        _health -= (int)Mathf.Round(dmg);
+        Debug.Log("Player Current damage taken " + dmg + "\n Current Health is: " + _health);
+
+        onPlayerDamaged?.Invoke(_health);
+        if(_health <= 0)
+        {
+            Debug.Log("Death to player");
+            onPlayerDeath?.Invoke();
+        }
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        
+    }
+
+}
